54b705829e6556acb9d744fc6eb75bdc9c26168a
Txagouuu <tiagomendon45@gmail.com>
2026-04-02 16:30:18 -0300

    feat: implement loop for subclass visualization

--- main.py
+++ main.py
@@ -12,3 +12,19 @@ chave_aberta = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, caminho_uninstall)
 num_subchaves = winreg.QueryInfoKey(chave_aberta)[0]
 
 print(num_subchaves)
+
+for i in range(num_subchaves):
+    subchave_nome = winreg.EnumKey(chave_aberta, i)
+    print(subchave_nome)
+
+    # Abrimos a subchave para ler os valores
+    subchave_aberta = winreg.OpenKey(chave_aberta, subchave_nome)
+
+    try:
+        display_name, _ = winreg.QueryValueEx(subchave_aberta, "DisplayName")
+        print(f"Nome do programa: {display_name}")
+    except FileNotFoundError:
+        print(f"error {i}")
+        pass  # A chave DisplayName pode não existir
+
+    winreg.CloseKey(subchave_aberta)
