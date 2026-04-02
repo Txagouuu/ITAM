d445cad51c56c1d60ec0e580347c7e18a0fe80ff
Txagouuu <tiagomendon45@gmail.com>
2026-04-02 16:10:42 -0300

    feat: add subclass search functionality

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ caminho_uninstall = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
 # 2. Abrimos a chave
 # HKEY_LOCAL_MACHINE é a raiz que abriga as configurações da máquina
 chave_aberta = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, caminho_uninstall)
+
+# 3. Listamos as subchaves
+num_subchaves = winreg.QueryInfoKey(chave_aberta)[0]
+
+print(num_subchaves)
