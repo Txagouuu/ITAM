a648b02c3a689d69d1a4cf5cf699757376b1c66a
Txagouuu <tiagomendon45@gmail.com>
2026-04-02 15:53:15 -0300

    feat: implement installed software extraction via winreg

---
+++ main.py
@@ -0,0 +1,9 @@
+import winreg
+import cryptography
+
+# 1. Definimos o caminho 
+caminho_uninstall = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+
+# 2. Abrimos a chave
+# HKEY_LOCAL_MACHINE é a raiz que abriga as configurações da máquina
+chave_aberta = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, caminho_uninstall)
